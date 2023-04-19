d5c4180aa835b17cd5b63f5975f3ab43aa42dea3
Chun-Yu Lo (Andy Lo) <chunyulo@arch.nctu.edu.tw>
2023-04-19 16:23:23 +0800

    Edit package.json

--- week-4/assignment-5/reactCounter/vite.config.js
+++ week-4/assignment-5/reactCounter/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/remote-assignments/week-4/assignment-5/reactcounter/",
   plugins: [react()]
 });
