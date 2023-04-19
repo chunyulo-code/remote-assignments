15d315a9e4e6cf3b2a2d11a90a79823515b48fc1
Chun-Yu Lo (Andy Lo) <chunyulo@arch.nctu.edu.tw>
2023-04-19 21:16:04 +0800

    Creating the state to the parameter  instead of

--- week-4/assignment-4/facebookRepos/src/App.jsx
+++ week-4/assignment-4/facebookRepos/src/App.jsx
@@ -1,37 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Repo from "./components/Repo";
 import "./styles/app.css";
 
 function App() {
   const [repos, setRepos] = useState([]);
-  const [repoCounts, setRepoCounts] = useState(5);
-  const url = `https://api.github.com/orgs/facebook/repos?per_page=${repoCounts}&page=1`;
+  const [page, setPage] = useState(1);
+  const url = `https://api.github.com/orgs/facebook/repos?per_page=5&page=${page}`;
+  const newReposRef = useRef(null);
 
   const repoElements = repos.map((repo, index) => {
     return <Repo repo={repo} key={index} />;
   });
 
-  function getMoreRepos() {
-    setRepoCounts((prevCount) => prevCount + 5);
-  }
-
   async function fetchRepos() {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      setRepos(data);
+      if (page == 1) {
+        setRepos(data);
+      } else {
+        setRepos((prev) => [...prev, ...data]);
+      }
     } catch (err) {
       console.error(err);
     }
   }
 
+  function getMoreRepos() {
+    setPage((prevPage) => prevPage + 1);
+  }
+
   useEffect(() => {
     fetchRepos();
-  }, [repoCounts, url]);
+  }, [page]);
 
   return (
     <div className="main">
-      <div className="repos">{repoElements}</div>
+      <div className="repos" ref={newReposRef}>
+        {repoElements}
+      </div>
       <div>
         <button className="moreButton" onClick={getMoreRepos}>
           More
