0721e1c93b370dedbd918f62170a2f11ffc3a71d
Chun-Yu Lo (Andy Lo) <chunyulo@arch.nctu.edu.tw>
2023-04-20 12:28:40 +0800

    Solve useEffect will run twice when component mounting -> unmounting -> mounting in React 18, the solution is add ignore param and clean-up function

--- week-4/assignment-4/facebookRepos/src/App.jsx
+++ week-4/assignment-4/facebookRepos/src/App.jsx
@@ -5,40 +5,41 @@ import "./styles/app.css";
 function App() {
   const [repos, setRepos] = useState([]);
   const [page, setPage] = useState(1);
+
   const url = `https://api.github.com/orgs/facebook/repos?per_page=5&page=${page}`;
-  const newReposRef = useRef(null);
 
   const repoElements = repos.map((repo, index) => {
     return <Repo repo={repo} key={index} />;
   });
-
-  async function fetchRepos() {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      if (page == 1) {
-        setRepos(data);
-      } else {
-        setRepos((prev) => [...prev, ...data]);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
 
   function getMoreRepos() {
     setPage((prevPage) => prevPage + 1);
   }
 
   useEffect(() => {
+    let ignore = false;
+    async function fetchRepos() {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        if (!ignore) {
+          setRepos((prev) => [...prev, ...data]);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
     fetchRepos();
+
+    return () => {
+      ignore = true;
+    };
   }, [page]);
 
   return (
     <div className="main">
-      <div className="repos" ref={newReposRef}>
-        {repoElements}
-      </div>
+      <div className="repos">{repoElements}</div>
       <div>
         <button className="moreButton" onClick={getMoreRepos}>
           More
