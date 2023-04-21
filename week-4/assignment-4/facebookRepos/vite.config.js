fb1575525a51cce669d6501f2b7348de47f546e4
Chun-Yu Lo (Andy Lo) <chunyulo@arch.nctu.edu.tw>
2023-04-22 01:48:59 +0800

    Find the best way to deploy dist to repo's subdirectory

--- week-4/assignment-4/facebookRepos/vite.config.js
+++ week-4/assignment-4/facebookRepos/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/remote-assignments/week-4/assignment-4/facebookRepos/"
+  base: "/remote-assignments/week-4/assignment-4/facebookrepos/",
+  plugins: [react()]
 });
