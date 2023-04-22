35fd3e80ccf57c45f72a5c4ceb78d264463e538f
Chun-Yu Lo (Andy Lo) <chunyulo@arch.nctu.edu.tw>
2023-04-22 10:22:32 +0800

    Correct all package.json file, the best way to deploy to repo's subdirectory is gh-pages -d dist --dest==week4/assignment-1or2or3/reactAppName

--- week-4/assignment-5/reactCounter/vite.config.js
+++ week-4/assignment-5/reactCounter/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/remote-assignments/week-4/assignment-5/reactCounter/",
+  base: "/remote-assignments/week-4/assignment-5/reactcounter/",
   plugins: [react()]
 });
